078228973618efc4914f18c49dad46e2a9aecdda
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-17 21:44:03 -0300

    Add project files.

---
+++ XF.Recursos/XF.Recursos/App_Code/OpcoesMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.Recursos.App_Code
+{
+    public class OpcoesMenu
+    {
+        public string Descricao { get; set; }
+        public string Icone { get; set; }
+        public Type TargetType { get; set; } //Aceita qualquer tipo de página
+    }
+}
---
+++ XF.Recursos/XF.Recursos/Menu/MenuView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Recursos.App_Code;
+
+namespace XF.Recursos.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuView : ContentPage
+	{
+        public ListView ListView { get { return lstMenu; } } //Nome lstMenu deve ser identica no XAML
+        public MenuView()
+        {
+            InitializeComponent();
+            //Carregando os itens do Menu
+            ObservableCollection<OpcoesMenu> menuItems = new ObservableCollection<OpcoesMenu>();
+            menuItems.Add(new OpcoesMenu
+            {
+                Descricao = "Home",
+                Icone = "Home.png",
+                TargetType = typeof(HomeView)
+            });
+            
+            lstMenu.ItemsSource = menuItems;
+        }
+
+
+    }
+}
